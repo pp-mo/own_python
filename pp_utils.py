f6c47aa9805d3693f0d219dae579287c61acad8f
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2015-10-28 18:04:09 +0000

    Small hacks for Python3 compliance.

--- pp_utils.py
+++ pp_utils.py
@@ -1,13 +1,21 @@
+from __future__ import print_function
+import six
+
+import datetime
+
 import numpy as np
 
+
 def dd(a, showall=False):
-  """ Extract only _public_ property names from dir(a). """
-  return [x for x in dir(a) if showall or x.find('_') != 0]
+    """ Extract only _public_ property names from dir(a). """
+    return [x for x in dir(a) if showall or x.find('_') != 0]
+
 
 def pl(l):
-  """ Print all elements of l. """
-  for x in l:
-    print x
+    """ Print all elements of l, each on a separate line. """
+    for x in l:
+        print(x)
+
 
 def pd(a, *args, **kwargs):
     """
@@ -25,6 +33,7 @@ def str_indent(str_lines, indent=4):
     return '\n'.join([indent_str + line
                       for line in str_lines.split('\n')])
 
+
 def inspect_lines(obj, indent=4, showall=False, showcalls=False):
     topline = '<{}>'.format(type(obj))
     attrs = [(name, getattr(obj, name))
@@ -36,7 +45,7 @@ def inspect_lines(obj, indent=4, showall=False, showcalls=False):
     if not showcalls:
         attrs = [(name, value) for name, value in attrs
                  if not callable(value)]
-    heads = ['{} {:s}'.format(name, type(value))
+    heads = ['{} {:s}'.format(name, str(type(value)))
              for name, value in attrs]
     values = [str(value) if not callable(value) else '()'
               for (name, value) in attrs]
@@ -48,6 +57,7 @@ def inspect_lines(obj, indent=4, showall=False, showcalls=False):
              for head, value in zip(heads, values)]
     return [topline] + lines
 
+
 def pi(obj, *args, **kwargs):
     """Inspect type+contents of attributes of object.
 
@@ -56,30 +66,31 @@ def pi(obj, *args, **kwargs):
     """
     pl(inspect_lines(obj, *args, **kwargs))
 
+
 def minmax(v):
     """ Return np.min(v) and np.max(v) as a pair. """
     return (np.min(v), np.max(v))
 
-import datetime
+
 class TimedBlock(object):
-    """ A class with contextmanager behaviour, to time a block of statements. 
-    New line
-    """
-
+    """A class with contextmanager behaviour, to time a block of statements."""
     def __init__(self, action=None):
-        """ Create contextmanager object, which after use contains the elapsed time result. 
+        """
+        Create contextmanager object.
 
-            Usage:
-              with TimedBlock() as t:
-                  <statements ...
-                  ...
-                  >
-              time_taken = t.seconds()
+        After use, this contains the elapsed time result.
 
-            Note: can use "action" as :
-             * True : print standard message 
-             * string : print custom message : string.format(seconds)
-             * callable : call(self).
+        Usage:
+          with TimedBlock() as t:
+              <statements ...
+              ...
+              >
+          time_taken = t.seconds()
+
+        Note: can use "action" as :
+         * True : standard message
+         * string : custom message, as in: "print action.format(seconds)"
+         * callable : call(self).
 
         """
         self.start_datetime = None
@@ -89,10 +100,12 @@ class TimedBlock(object):
         elif hasattr(action, '__call__'):
             print_call = action
         else:
-            if not isinstance(action, basestring):
+            if not isinstance(action, six.string_types):
                 action = 'Time taken : {:12.6f} seconds'
+
             def _inner_print(self):
-                print action.format(self.seconds())
+                print(action.format(self.seconds()))
+
             print_call = _inner_print
         self.print_call = print_call
 
@@ -108,13 +121,14 @@ class TimedBlock(object):
             self.print_call(self)
 
     def seconds(self):
-        """ Our elapsed time in seconds. """ 
+        """Our elapsed time in seconds."""
         return self._seconds
+
 
 def pwhich(name):
     module = __import__(name)
-    print name, '.__file__ = ',module.__file__
-    print name, '.__version__ = ', getattr(module, '__version__', '(None)')
+    print(name, '.__file__ = ', module.__file__)
+    print(name, '.__version__ = ', getattr(module, '__version__', '(None)'))
 
 
 def array_difference_stats(array_a, array_b, min_reldiff=1e-20):
@@ -126,24 +140,25 @@ def array_difference_stats(array_a, array_b, min_reldiff=1e-20):
     absmags = np.max(absmags, min_reldiff * np.max(absmags))
     reldiffs = absdiffs / absmags
     result = OrderedDict(
-         [(name, OrderedDict(
-             [(op.__name__, op(array))
-              for op in (ma.min, ma.max, ma.mean, ma.median)]))
-          for array, name in zip(
-              [array_a, diffs, absdiffs, reldiffs],
-              ['values', 'diffs', 'abs-diffs', 'rel-diffs'])])
+        [(name, OrderedDict(
+            [(op.__name__, op(array))
+             for op in (ma.min, ma.max, ma.mean, ma.median)]))
+         for array, name in zip(
+             [array_a, diffs, absdiffs, reldiffs],
+             ['values', 'diffs', 'abs-diffs', 'rel-diffs'])])
     return result
+
 
 def format_array_difference_stats(array_a, array_b, **kwargs):
     results = array_difference_stats(array_a, array_b, **kwargs)
     strs = []
     for typename, opsdict in results.iteritems():
         strs.append('{:>10s}:  {}'.format(typename, ',  '.join([
-                       '{:>6s}={:>12s}'.format(opname, '{:>7g}'.format(value))
-                       for opname, value in opsdict.iteritems()])))
+            '{:>6s}={:>12s}'.format(opname, '{:>7g}'.format(value))
+            for opname, value in opsdict.iteritems()])))
     return strs
+
 
 def show_array_difference_stats(array_a, array_b, **kwargs):
     strings = format_array_difference_stats(array_a, array_b, **kwargs)
-    print '\n'.join(strings)
-
+    print('\n'.join(strings))
